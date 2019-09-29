13526592ac8c9de605131815326711c8a8005cd4
Certinax <s319217@oslomet.no>
2019-09-28 21:36:57 -0700

    Added a comment plus updated format with vscode

--- public/project2/js/main.js
+++ public/project2/js/main.js
@@ -1,8 +1,9 @@
-$(function () {
-    setYearInFooter();
+// on ready function,- jQuery-way
+$(function() {
+  setYearInFooter();
 });
 
 const setYearInFooter = () => {
-    const curDate = new Date().getFullYear();
-    $("#currentDate").html(curDate);
-}
+  const curDate = new Date().getFullYear();
+  $("#currentDate").html(curDate);
+};
