b6d3e71daa0934e95aa40239d6b1a3696d6b2b58
Certinax <s319217@oslomet.no>
2019-10-09 22:01:31 -0700

    Completed midterm practice and started on a default layout

---
+++ public/practice/defaultLayout/js/helperfunctions.js
@@ -0,0 +1,32 @@
+/**
+ * Function that accepts any html element and access the numeric value
+ * @param element is any HTML-element
+ * @returns any number
+ */
+function convertToNumber(element) {
+  if (element.html()) {
+    // return parseInt(element.html().replace(/\D/g, ""));
+    return Number(element.html().replace(/[^0-9,.]+/, ""));
+  }
+  return 0;
+}
+
+/**
+ * Basic ajax call
+ */
+function ajaxMe() {
+  $.ajax({
+    url: "",
+    type: "GET", // Can also use POST
+    dataType: "json", // XML is also viable
+    data: {
+      // Optional
+      param1: "param1",
+      param2: "param2"
+    },
+    success: function(result, status) {
+      console.log(result); // Data from API
+      console.log(status); // Statuscode from API
+    }
+  });
+}
--- public/practice/midterm/js/main.js
+++ public/practice/midterm/js/main.js
@@ -2,6 +2,12 @@ const product = $("#product");
 const unitprice = $("#unitprice");
 const quantity = $("#quantity");
 const productTotal = $("#productTotal");
+const discountSum = $("#discountSum");
+const shipping = $("#shipping");
+const subtotal = $("#subTotal");
+const tax = $("#tax");
+const total = $("#total");
+const shippingMethod = $("#shippingMethod");
 
 const promoButton = $("#promoButton");
 const promoCode = $("#promoCode");
@@ -17,24 +23,104 @@ $(function() {
       const { productName, price, qty } = data[randItem];
 
       product.html(productName);
-      unitprice.html(price);
+      unitprice.html(`$${price}`);
       quantity.val(qty);
 
       const pTotal = price * qty;
       productTotal.html(`${pTotal}`);
+      updateValues();
     }
   });
 });
 
 // Changes product total price upon quantity change
 quantity.on("change", () => {
-  productTotal.html(quantity.val() * unitprice.html());
+  productTotal.html(quantity.val() * convertToNumber(unitprice));
+  updateValues();
 });
 
-promoButton.on("click", () => {
-  console.log("PROMO CHECKING");
-
-  console.log(promoCode.val());
-
-  let discount = promoMe();
+shippingMethod.on("change", function() {
+  updateValues();
 });
+
+promoButton.on("click", function() {
+  promoMe();
+});
+
+function promoMe() {
+  $.ajax({
+    url: "https://cst336.herokuapp.com/projects/api/promo_codes.php",
+    type: "GET",
+    dataType: "json",
+    data: {
+      promoCode: promoCode.val().toLowerCase()
+    },
+    success: function(res, status) {
+      if (!res) {
+        promoError();
+      } else {
+        promoSuccess(res.discount);
+        updateDiscount(res.discount);
+        updateValues();
+      }
+    }
+  });
+}
+
+function promoError() {
+  $("#discountMsg").attr("class", "text-danger");
+  $("#discountMsg").html("Invalid promocode");
+}
+
+function promoSuccess(discount) {
+  $("#discountMsg").attr("class", "text-primary");
+  $("#discountMsg").html(`Your discount: ${discount}%`);
+}
+
+function updateDiscount(discount) {
+  const discount_dec = discount / 100;
+  discountSum.html(`$-${productTotal.html() * discount_dec}`);
+}
+
+function convertToNumber(element) {
+  if (element.html()) {
+    // return parseInt(element.html().replace(/\D/g, ""));
+    return Number(element.html().replace(/[^0-9,.]+/, ""));
+  }
+  return 0;
+}
+
+function updateTotal() {
+  const subtotalVal = convertToNumber(subtotal);
+  const taxVal = convertToNumber(tax);
+
+  const newTotal = subtotalVal + taxVal;
+  total.html(`$${newTotal}`);
+}
+
+function updateShipping() {
+  if (shippingMethod.val() != 0) {
+    shipping.html(`$${shippingMethod.val()}`);
+  }
+}
+
+function updateSubtotal() {
+  const subtotalVal =
+    convertToNumber(productTotal) -
+    convertToNumber(discountSum) +
+    convertToNumber(shipping);
+
+  subtotal.html(`$${subtotalVal}`);
+}
+
+function updateTax() {
+  const taxVal = convertToNumber(subtotal) * 0.1;
+  tax.html(`$${taxVal.toFixed(2)}`);
+}
+
+function updateValues() {
+  updateShipping();
+  updateSubtotal();
+  updateTax();
+  updateTotal();
+}
